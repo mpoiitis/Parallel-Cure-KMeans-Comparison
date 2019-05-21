cf4e2cff0472591f91450ca59fd0c20a4d98925d
Marinos Poiitis <marinospoet@gmail.com>
2019-05-21 12:54:40 +0300

    Add completed MinHeap

--- src/main/scala/MinHeap.scala
+++ src/main/scala/MinHeap.scala
@@ -28,16 +28,18 @@ class MinHeap(size: Int){
     Swap two elements in the heap array
    */
   def swap(i: Int, j: Int): Unit ={
-    val temp : Cluster = this.minHeap(i)
-    this.minHeap(i) = this.minHeap(j)
-    this.minHeap(j) = temp
+    val temp : Cluster = minHeap(i)
+    minHeap(i) = minHeap(j)
+    minHeap(j) = temp
 
   }
 
+  /*
+    Inserts a cluster into the MinHeap
+   */
   def insert(cluster: Cluster): Unit = {
     if (currentSize == size) {
-      println("Overflow: Could not insert key!")
-      return
+      throw new Exception("Overflow: Could not insert key!")
     }
 
     // insert new cluster at the end
@@ -46,16 +48,93 @@ class MinHeap(size: Int){
     minHeap(i) = cluster
 
     // fix min heap property if it is violated
-    var parentCluster = this.minHeap(this.parent(i))
-    var childCluster = this.minHeap(i)
+    var parentCluster = minHeap(this.parent(i))
+    var childCluster = minHeap(i)
     while (i !=0 &&  Utils.clusterDistance(parentCluster, parentCluster.closest)> Utils.clusterDistance(childCluster, childCluster.closest) ){
       this.swap(i, this.parent(i))
       i = this.parent(i)
 
-      parentCluster = this.minHeap(this.parent(i))
-      childCluster = this.minHeap(i)
+      parentCluster = minHeap(this.parent(i))
+      childCluster = minHeap(i)
     }
   }
 
+  /*
+    Removes the min (root) element from the MinHeap
+   */
+  def extractMin(): Cluster = {
+    if (currentSize <= 0){
+      throw new Exception("Heap is already empty!")
+    }
+    if (currentSize == 1){
+      currentSize -= 1
+      return minHeap(0)
+    }
+
+    // store the root value to return and find the new root
+    val root : Cluster = minHeap(0)
+    minHeap(0) = minHeap(currentSize-1)
+    currentSize -= 1
+    this.heapify(0)
+
+    root
+  }
+
+  /*
+    Heapify a subtree with the given index as root
+    Assumes that the subtrees are already heapified
+   */
+  def heapify(i: Int): Unit = {
+    val left : Int = this.leftChild(i)
+    val right : Int = this.rightChild(i)
+    var smallest : Int = i
+
+    // find the smallest between left, right children and current node
+    val leftCluster = minHeap(left)
+    val rightCluster = minHeap(right)
+    var parentCluster = minHeap(i)
+
+    if (left < currentSize &&
+      Utils.clusterDistance(leftCluster, leftCluster.closest) < Utils.clusterDistance(parentCluster, parentCluster.closest)){
+      smallest = left
+    }
+
+    if (right < currentSize &&
+      Utils.clusterDistance(rightCluster, rightCluster.closest) < Utils.clusterDistance(minHeap(smallest), minHeap(smallest).closest)){
+      smallest = right
+    }
+
+    // if smallest is different than the current node, swap them and recurse
+    if (smallest != i) {
+      this.swap(i, smallest)
+      heapify(smallest)
+    }
+  }
+
+  /*
+    Remove the cluster of the specific index from the MinHeap
+   */
+  def deleteKey(i: Int): Cluster = {
+
+    // make parameter var
+    var nodeIndex = i
+
+    // declare the best possible minimum as the distance for the cluster we want to remove
+    // so as to move up the whole MinHeap
+    val delClusterDistance : Double = scala.Double.MinValue
+    var parentCluster = minHeap(this.parent(nodeIndex))
+
+    //move up the whole MinHeap
+    while ( nodeIndex != 0 && Utils.clusterDistance(parentCluster, parentCluster.closest) > delClusterDistance){
+      swap(nodeIndex, this.parent(nodeIndex))
+      nodeIndex = this.parent(nodeIndex)
+
+      parentCluster = minHeap(this.parent(nodeIndex))
+    }
+
+    // the element we wanted to remove has now reached the root of the MinHeap
+    // so extract the root
+    this.extractMin()
+  }
 
 }

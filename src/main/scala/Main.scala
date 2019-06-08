ad99271130c9e50cd5e0444aa0b9184cd864a7b3
Marinos Poiitis <marinospoet@gmail.com>
2019-06-08 21:06:48 +0300

    Add kmeans algorithm

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,8 +4,10 @@
 
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.evaluation.ClusteringEvaluator
+import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.functions.min
 
 object Main {
@@ -20,35 +22,76 @@ object Main {
     Logger.getRootLogger.setLevel(Level.WARN)
     import ss.implicits._
 
+    // CLI ARGUMENTS
+    val numIntermediateClusters = args(2).toInt
+    val numClusters = args(0).toInt
+    val sampleSize = args(1).toDouble
+    val numRepresentatives = args(3).toInt
+    val shrinkFactor = args(4).toDouble
 
-//    val files = new java.io.File("data/").listFiles.filter(_.getName.endsWith(".txt"))
-//
-//    var data: DataFrame = null
-//    for (file <- files) {
-//      val fileDf= ss.read.csv(file.toString)
-//      if (data!= null) {
-//        data= data.union(fileDf)
-//      } else {
-//        data= fileDf
-//      }
-//    }
-//
-//    val numOfExamples = data.count()
+    // READ DATA
+
+    val files = new java.io.File("data/").listFiles.filter(_.getName.endsWith(".txt"))
+
+    var data: DataFrame = null
+    for (file <- files) {
+      val fileDf= ss.read.option("inferSchema","true").csv(file.toString)
+      if (data!= null) {
+        data= data.union(fileDf)
+      } else {
+        data= fileDf
+      }
+    }
+    data = data.toDF("x", "y").cache()
+
+    val numOfExamples = data.count()
 //    val ratio = 0.01
 //    data = data.sample(ratio)
-//
-//    println(numOfExamples)
+
+    println("Number of instances in dataset: " + numOfExamples)
 //    println(data.count())
 
-    // Cluster sample points hierarchically and in a parallel fashion using SHAS
-    val data: DataFrame = ss.read.option("inferSchema","true").csv("data/data1.txt").toDF("x", "y").sample(args(1).toDouble)
-    val shas = new SHAS(data, splits = 4, ss = ss)
-    val clusters: Array[(Array[Point], Int)] = shas.run(numClusters = args(2).toInt)
-    val cure = new Cure(clusters, args(0).toInt, args(4).toDouble, args(3).toInt, ss)
-    val finalClusters: RDD[Cluster] = cure.run()
+//    val data: DataFrame = ss.read.option("inferSchema","true").csv("data/data1.txt").toDF("x", "y").sample(sampleSize).cache()
 
-    finalClusters.foreach(println)
-//    Thread.sleep(30000000)
+    // KMEANS
+
+    println("Running KMeans...")
+    // creating features column
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("x","y"))
+      .setOutputCol("features")
+    val transformedData = assembler.transform(data)
+
+    // the actual model
+    val kmeans = new KMeans()
+      .setK(numIntermediateClusters)
+      .setSeed(1L)
+      .setFeaturesCol("features")
+    val model = kmeans.fit(transformedData)
+
+
+    val predictions = model.transform(transformedData)
+
+    // Evaluate clustering by computing Silhouette score
+    val evaluator = new ClusteringEvaluator()
+    val silhouette = evaluator.evaluate(predictions)
+    println("Silhouette with squared euclidean distance = " + silhouette)
+
+    // Shows the result.
+    println("KMeans Cluster Centers: ")
+    model.clusterCenters.foreach(println)
+
+//    // CURE
+//      println("Running SHAS...")
+//    // Cluster sample points hierarchically and in a parallel fashion using SHAS
+//    val shas = new SHAS(data, splits = 4, ss = ss)
+//    val clusters: Array[(Array[Point], Int)] = shas.run(numClusters = numIntermediateClusters)
+//    println("Running CURE...")
+//    val cure = new Cure(clusters, numClusters, shrinkFactor, numRepresentatives, ss)
+//    val finalClusters: RDD[Cluster] = cure.run()
+//
+//    finalClusters.foreach(println)
+
     ss.stop()
   }
 
